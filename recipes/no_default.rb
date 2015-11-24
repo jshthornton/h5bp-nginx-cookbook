8a12aae9ee8d794142ce725d7c41dacffaab5f20
Joshua Thornton <joshua@techinasia.com>
2015-11-24 13:14:06 +0800

    Create new site, disable default.

--- recipes/no_default.rb
+++ recipes/no_default.rb
@@ -1,14 +1,10 @@
 # No default site
-template '/etc/nginx/sites-available/no-default' do
-  source 'no-default.erb'
-  owner 'root'
-  group 'root'
-  mode '0755'
+nginxsite_create 'no-default' do
+  template "no-default.erb"
+  cookbook 'nginxh5bp'
+  variables {}
+  enable true
 end
 
-file "/etc/nginx/sites-enabled/default" do
-  action :delete
-  only_if do
-    File.exists?("/etc/nginx/sites-enabled/default")
-  end
+nginxsite_disable '000-default' do
 end
